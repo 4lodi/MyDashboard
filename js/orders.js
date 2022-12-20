7630d8cbcd840da73c9147dbcbbdd5604e673346
Luis Mondaca <luis.mondaca03@gmail.com>
2022-12-20 16:35:17 -0300

    first commit

---
+++ js/orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+  {
+    productName: "producto 1",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "producto 2",
+    productNumber: "85631",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "producto 3",
+    productNumber: "85631",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "producto 4",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "producto 5",
+    productNumber: "85631",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "producto 6",
+    productNumber: "85631",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "producto 7",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "producto 8",
+    productNumber: "85631",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
